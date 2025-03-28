6442f0aa986ed38bfc3610742d02fae8fae17c44
lumintriet <lumintriet@gmail.com>
2025-03-28 03:33:31 +0000

    After setup commit.

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+  semi: false,
+  singleQuote: false,
+  trailingComma: 'all',
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindStylesheet: "./src/styles.css",
+  tailwindFunctions: ["clsx", "cva"]
+}
+
+export default config
